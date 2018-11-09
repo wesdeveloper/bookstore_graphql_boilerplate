1f7e7e3d6913cde55d5b5968e19e84e20ffc5151
weslopes <wesleyalopes2010@gmail.com>
2018-11-08 22:15:49 -0200

    Setup babelrc editorconfig and eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+      "max-len": 1,
+      "no-console": 0,
+      "no-undef": 0,
+      "no-underscore-dangle": 0,
+      "no-param-reassign": 0,
+      "eol-last": 2,
+      "camelcase": 0,
+      "consistent-return": 0,
+      "global-require": 0,
+      "implicit-arrow-linebreak": 0,
+    }
+};
